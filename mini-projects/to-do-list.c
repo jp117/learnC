a97ec205facefc6e5832368853b3addccbfca391
jp117 <john.paradise117@gmail.com>
2025-06-28 22:33:11 -0400

    added commands for to do list

--- mini-projects/to-do-list.c
+++ mini-projects/to-do-list.c
@@ -9,5 +9,28 @@ int main() {
     printf("Welcome to your To-Do List\n");
     printf("Commands: add [task], list, delete [id], quit\n");
 
+    while (1)
+    {
+        printf("> ");
+        if (fgets(input, MAX_INPUT, stdin) == NULL) break;
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strncmp(input, "add ", 4) == 0) {
+            printf("Adding task: %s\n", input + 4);
+        } else if (strcmp(input, "list") == 0) {
+            printf("Listing tasks...\n");
+        } else if (strncmp(input, "delete ", 7) == 0) {
+            printf("Deleting task ID: %s\n", input + 7);
+        } else if (strcmp(input, "quit") == 0 || strcmp(input, "exit") == 0) {
+            printf("Goodbye!\n");
+            break;
+        } else {
+            printf("Unknown command.\n");
+        }
+
+    }
+    
+
     return 0;
 }
