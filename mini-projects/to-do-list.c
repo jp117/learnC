1f0189c29a2e85aef532f1d7310046bd11ee71fc
jp117 <john.paradise117@gmail.com>
2025-06-28 22:14:33 -0400

    started cli to-do list micro-project

---
+++ mini-projects/to-do-list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT 100
+
+int main() {
+    char input[MAX_INPUT];
+
+    printf("Welcome to your To-Do List\n");
+    printf("Commands: add [task], list, delete [id], quit\n");
+
+    return 0;
+}
---
+++ mini-projects/to-do-list.md
@@ -0,0 +1,4 @@
+1. **To-Do CLI App**
+    - Add, list, delete tasks (in memory)
+    - Store in a linked list
+    - Stretch: save/load from file
